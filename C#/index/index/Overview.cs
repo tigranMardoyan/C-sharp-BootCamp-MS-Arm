8f868fae5e49d634cab87e1482b16be538bfb674
Tigran <101590375+tigranMardoyan@users.noreply.github.com>
2023-05-15 23:20:03 +0400

    Add files via upload

---
+++ C#/index/index/Overview.cs
@@ -0,0 +1,87 @@
+﻿      // at //
+
+int @bool = 1256000;
+Console.WriteLine(@bool);
+
+//specific case aplied to -@- //
+
+int @userCount = 500000000;
+Console.WriteLine(userCount);
+
+
+string @userFirstName = "Karen Smith";
+Console.WriteLine(userFirstName);
+
+
+// Casting //
+
+int countNum = 500;
+byte xyz = 0;
+
+xyz = (byte)(countNum);
+
+
+
+// This Case above successfully built since of the numeric enough being //
+
+
+// Inc / Dec
+
+/*byte num1 = 1;
+
+byte num3 = ++num1;
+
+byte num2;
+
+
+Console.WriteLine(num3);
+Console.WriteLine(num2 = num1--);
+
+Console.WriteLine(num2 = --num1);
+Console.ReadLine();
+
+
+
+
+
+byte variable = 0;
+
+variable = (byte)(variable + 5);
+
+Console.WriteLine(variable);
+
+variable /= 5;
+
+Console.WriteLine(variable); */
+
+
+
+
+// Math Experssions //
+
+int numX = 500;
+double numY = 0.5;
+
+/*Console.WriteLine(numX + numY);
+Console.WriteLine(numX - numY);
+Console.WriteLine(numX / numY);
+Console.WriteLine(numX * numY);
+Console.WriteLine(numX % numY); */
+
+
+double number = Math.Pow(5, 33);
+Console.WriteLine(number);
+
+double numberSqrt = Math.Sqrt(25);
+Console.WriteLine(numberSqrt);
+
+
+
+
+
+
+
+
+
+
+
